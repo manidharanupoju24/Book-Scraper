e1020c3b1841dcc6f4cd506a4b5f171409ada49d
uchihaitachi24 <manidharanupoju@gmail.com>
2018-08-18 16:34:38 +0530

    Added menu for user interaction

--- menu.py
+++ menu.py
@@ -1,6 +1,16 @@
 from app import books
 
 
+USER_CHOICE = ''' Enter one of the following
+
+- 'b' to look at 5-star rating books
+- 'c' to look at the cheapest books
+- 'n' to just get the next available book on the page
+- 'q' to quit
+
+Enter your choice: '''
+
+
 def print_best_books():
     best_books = sorted(books, key=lambda x: x.rating * -1)[:10]
     for book in best_books:
@@ -13,6 +23,30 @@ def print_cheapest_books():
         print(book)
 
 
-#print_best_books()
+book_generator = (x for x in books)
 
-print_cheapest_books()
+
+def get_next_book():
+    print(next(book_generator))
+
+
+def menu():
+    user_input = input(USER_CHOICE)
+
+    while user_input != 'q':
+        if user_input == 'b':
+            print_best_books()
+
+        elif user_input == 'c':
+            print_cheapest_books()
+
+        elif user_input == 'n':
+            get_next_book()
+
+        else:
+            print('Please choose a valid command.\n')
+
+        user_input = input(USER_CHOICE)
+
+
+menu()
